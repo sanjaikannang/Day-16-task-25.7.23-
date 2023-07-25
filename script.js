1c92b150820d8a9709c278a80a29bdc7e8d714dc
sanjaikannang <sanjaikannang@gmail.com>
2023-07-25 09:00:07 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,45 @@
+let timer=10;
+let time = document.getElementById('display');
+
+setTimeout(() => {
+    time.innerHTML = timer--;
+    setTimeout(() =>{
+        time.className = "countdown";
+        time.innerHTML = timer--;
+        setTimeout(() =>{
+            time.className = "countdown";
+            time.innerHTML = timer--;
+            setTimeout(() =>{
+                time.className = "countdown";
+                time.innerHTML = timer--;
+                setTimeout(() =>{
+                    time.className = "countdown";
+                    time.innerHTML = timer--;
+                    setTimeout(() =>{
+                        time.className = "countdown";
+                        time.innerHTML = timer--;
+                        setTimeout(() =>{
+                            time.className = "countdown";
+                            time.innerHTML = timer--;
+                            setTimeout(() =>{
+                                time.className = "countdown";
+                                time.innerHTML = timer--;
+                                setTimeout(() =>{
+                                    time.className = "countdown";
+                                    time.innerHTML = timer--;
+                                    setTimeout(() =>{
+                                        time.className = "countdown";
+                                        time.innerHTML = timer--;
+                                        setTimeout(() =>{
+                                            time.innerHTML = "Happy Independence Day"
+                                            },1000)
+                                    },1000)
+                                },1000)
+                            },1000)
+                        },1000)
+                    },1000)
+                },1000)
+            },1000)     
+        },1000)
+    },1000)
+}, 1000); 
